85410e0e5c7d87d3218dcc5f3a156364b07730ce
Rawier <alexam211@hotmail.com>
2025-07-02 21:34:49 -0600

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GIE-page/', // Asegura que las rutas de los activos sean correctas en producción
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
